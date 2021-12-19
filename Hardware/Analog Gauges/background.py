acb56de7ba6d439c4e9195d314de702b55557269
Eduard Schmidt <Eduard.Schmidt@posteo.net>
2021-12-19 12:42:13 +0100

    started with the Background SVG File

--- Hardware/Analog Gauges/background.py
+++ Hardware/Analog Gauges/background.py
@@ -1,5 +1,40 @@
 # importing pycairo
+#To install p<cairo we also need teh tependencies of the C Headers: 
+# > brew install cairo pkg-config
 import cairo
+import math
+
+
+
+def drawGaugeBackground(drawingContext, intMin, intMax, strDescriptioin):
+	"""
+	Draw the gauge backgrounds
+	"""
+
+	startX = 0.5
+	startY = 0.5
+	radius = 0.4
+
+	arcWidthOffset = (math.pi * 0.2)
+
+	angleStart = math.pi + arcWidthOffset
+	angleEnd = (2 * math.pi) - arcWidthOffset
+
+	lineWidth = 0.01
+
+	#Draw the arc
+	context.arc(startX, startY, radius, angleStart, angleEnd)
+	context.set_source_rgb(0, 0, 0)
+	context.set_line_width(lineWidth)
+
+	context.select_font_face("Georgia", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+	context.set_font_size(13)
+
+	context.move_to(startX, startY)
+	context.show_text(strDescriptioin)
+
+	context.stroke()
+
 
 # creating a SVG surface
 # here geek is file name & 700, 700 is dimension
@@ -8,35 +43,12 @@ with cairo.SVGSurface("geek.svg", 700, 700) as surface:
 	# creating a cairo context object
 	context = cairo.Context(surface)
 
-	# creating a rectangle(square) for left eye
-	context.rectangle(100, 100, 100, 100)
-
-	# creating a rectangle(square) for right eye
-	context.rectangle(500, 100, 100, 100)
-
-	# creating position for the curves
-	x, y, x1, y1 = 0.1, 0.5, 0.4, 0.9
-	x2, y2, x3, y3 = 0.4, 0.1, 0.9, 0.6
-
 	# setting scale of the context
 	context.scale(700, 700)
 
-	# setting line width of the context
-	context.set_line_width(0.04)
+	drawGaugeBackground(context, 0, 20, "Upload")
 
-	# move the context to x,y position
-	context.move_to(x, y)
-
-	# draw the curve for smile
-	context.curve_to(x1, y1, x2, y2, x3, y3)
-
-	# setting color of the context
-	context.set_source_rgba(0.4, 1, 0.4, 1)
-
-	# stroke out the color and width property
-	context.stroke()
 
 
 # printing message when file is saved
 print("File Saved")
-
