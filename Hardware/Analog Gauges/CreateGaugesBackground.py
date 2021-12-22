a523bbf001a15caae6d95e2765d3580d60597ad5
Eduard Schmidt <Eduard.Schmidt@posteo.net>
2021-12-23 00:37:12 +0100

    Updated the Gauge creation

--- Hardware/Analog Gauges/CreateGaugesBackground.py
+++ Hardware/Analog Gauges/CreateGaugesBackground.py
@@ -4,24 +4,158 @@
 import cairo
 import math
 
+#Define the metrics of the gauge
+MAX_DOWNLOAD_SPEED = 200
+MAX_UPLOAD_SPEED = 30
 
-def drawText(ctx, string, pos, theta = 0.0, face = 'Georgia', font_size = 18):
-    ctx.save()
+BACKGROUND_SIZE_X = 650
+BACKGROUND_SIZE_Y = 400
 
-    # build up an appropriate font
-    ctx.select_font_face(face , cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-    ctx.set_font_size(font_size)
-    fascent, fdescent, fheight, fxadvance, fyadvance = ctx.font_extents()
-    x_off, y_off, tw, th = ctx.text_extents(string)[:4]
-    nx = -tw/2.0
-    ny = fheight/2
 
-    ctx.translate(pos[0], pos[1])
-    ctx.rotate(theta)
-    ctx.translate(nx, ny)
-    ctx.move_to(0,0)
-    ctx.show_text(string)
-    ctx.restore()
+#Define the style of the gauges  
+FONT_LABEL = "Helvetica"
+FONT_LABEL_SIZE = 40
+FONT_DESCRIPTION = "Helvetica"
+FONT_DESCRIPTION_SIZE = 48
+
+def main():
+	"""Main programm"""
+
+	#Draw the upload gauge background
+	with cairo.SVGSurface("upload.svg", BACKGROUND_SIZE_X, BACKGROUND_SIZE_Y) as surface:
+
+		# creating a cairo context object asd asd 
+		context = cairo.Context(surface)
+		drawGaugeBackground(context, MAX_UPLOAD_SPEED, "UP",  6, BACKGROUND_SIZE_X, BACKGROUND_SIZE_Y)
+
+	#Draw the download gauge background
+	with cairo.SVGSurface("download.svg", BACKGROUND_SIZE_X, BACKGROUND_SIZE_Y) as surface:
+
+		# creating a cairo context object
+		context = cairo.Context(surface)
+		drawGaugeBackground(context, MAX_DOWNLOAD_SPEED, "DOWN", 4, BACKGROUND_SIZE_X, BACKGROUND_SIZE_Y)
+
+	# printing message when files are saved
+	print("Files Saved")
+
+
+def drawGaugeBackground(drawingContext, intMax, description, devidedBy, sizeBackgroundX, sizeBackgroundY):
+	"""
+	Draw the gauge backgrounds
+	"""
+
+	startX = sizeBackgroundX/2
+	startY = sizeBackgroundY
+	radius = 320
+
+
+	lineWidth = 4
+
+	drawingContext.set_source_rgb(0, 0, 0)
+	drawingContext.set_line_width(lineWidth)
+
+	#------------------------------------------
+	#Draw the frame (TOP)
+	drawingContext.move_to(0, 0)
+	drawingContext.line_to(sizeBackgroundX, 0)
+
+	#Draw the frame (Left)
+	drawingContext.move_to(0, 0)
+	drawingContext.line_to(0, sizeBackgroundY - 90)
+
+	#Draw the frame (Bottom)
+	drawingContext.move_to(0, sizeBackgroundY - 90)
+	drawingContext.line_to(50, sizeBackgroundY - 90)
+
+	drawingContext.move_to(50, sizeBackgroundY - 90)
+	drawingContext.line_to(100, sizeBackgroundY)
+
+	drawingContext.move_to(100, sizeBackgroundY)
+	drawingContext.line_to(200, sizeBackgroundY)
+
+	drawingContext.move_to(200, sizeBackgroundY)
+	drawingContext.line_to(250, sizeBackgroundY - 100)
+
+	drawingContext.move_to(250, sizeBackgroundY - 100)
+	drawingContext.line_to(400, sizeBackgroundY - 100)
+
+	drawingContext.move_to(400, sizeBackgroundY - 100)
+	drawingContext.line_to(450, sizeBackgroundY)
+
+	drawingContext.move_to(450, sizeBackgroundY)
+	drawingContext.line_to(550, sizeBackgroundY)
+
+	drawingContext.move_to(550, sizeBackgroundY)
+	drawingContext.line_to(600, sizeBackgroundY - 90)
+
+	drawingContext.move_to(550, sizeBackgroundY)
+	drawingContext.line_to(600, sizeBackgroundY - 90)
+
+	drawingContext.move_to(600, sizeBackgroundY - 90)
+	drawingContext.line_to(sizeBackgroundX, sizeBackgroundY - 90)
+
+	#Draw the frame (Right)
+	drawingContext.move_to(sizeBackgroundX, 0)
+	drawingContext.line_to(sizeBackgroundX, sizeBackgroundY - 90)
+
+
+	drawingContext.stroke()
+
+	#------------------------------------------
+	#Draw the arc
+
+	angleOffsetDeg = 45
+
+	angleStartSinusDeg = angleOffsetDeg
+	angleStartArcDeg = 270 - angleOffsetDeg
+	angleStartSinusRad = ((math.pi*2)/360) * angleStartSinusDeg
+	angleStartArcRad = ((math.pi*2)/360) * angleStartArcDeg
+
+	angleEndSinusDeg = -angleOffsetDeg
+	angleEndArcDeg = 270 + angleStartSinusDeg
+	angleEndSinusRad = ((math.pi*2)/360) * angleEndSinusDeg
+	angleEndArcRad = ((math.pi*2)/360) * angleEndArcDeg
+
+
+	drawingContext.arc(startX, startY, radius, angleStartArcRad, angleEndArcRad)
+	drawingContext.stroke()
+
+	#------------------------------------------
+	#Draw the lines and labels of the line
+	count = devidedBy
+
+	angleSumDeg = 2 * angleOffsetDeg
+	angleDelta = angleSumDeg / (count * 5)
+	speedDelta = intMax / count
+	angleDeg = angleStartSinusDeg
+	speedLabel = 0
+
+	for idx in range(5 * count + 1):
+		rad = ((math.pi*2)/360) * angleDeg
+		
+		if int(idx/5) == idx/5:
+			length = 30
+			lineWidth = 5
+			label = str(int(speedLabel))
+			speedLabel = speedLabel + speedDelta
+		else:
+			length = 15
+			lineWidth = 2
+			label = ""
+
+		drawLine(drawingContext, startX, startY, radius, rad, length, lineWidth, label)
+		angleDeg = angleDeg - angleDelta
+
+	#------------------------------------------
+	#Write the description
+	drawText(drawingContext, description, (startX, startY - ((radius/8)*6)), 0, FONT_DESCRIPTION, FONT_DESCRIPTION_SIZE)
+
+	#Write the unit
+	drawText(drawingContext, "[Mbps]", (startX, startY - ((radius/8)*6) + 55), 0, FONT_DESCRIPTION, FONT_DESCRIPTION_SIZE - 12)
+
+	#------------------------------------------
+	#Write to the svg
+	drawingContext.stroke()
 
 def drawLine(context, startX, startY, radius, angle, lineLength, lineWidth, label):
 	"""
@@ -45,114 +179,35 @@ def drawLine(context, startX, startY, radius, angle, lineLength, lineWidth, labe
 	context.stroke()
 
 	#Set the Text to the 
-	radiusText = radiusEnd + 16
-	delta_x2 = math.sin(angle) * radiusText
-	delta_y2 = math.sqrt(radiusText * radiusText - delta_x2 * delta_x2)
-
 	#If the label is available write it on top of the line
 	if len(label) > 0:
-		drawText(context, label, (startX - delta_x2, startY - delta_y2), (2 * math.pi) - angle, "Georgia", 24)
+
+		radiusText = radiusEnd + 25
+		delta_x2 = math.sin(angle) * radiusText
+		delta_y2 = math.sqrt(radiusText * radiusText - delta_x2 * delta_x2)
+
+		drawText(context, label, (startX - delta_x2, startY - delta_y2), (2 * math.pi) - angle, FONT_LABEL, FONT_LABEL_SIZE)
+
+def drawText(ctx, string, pos, theta = 0.0, face = 'Georgia', font_size = 18):
+	ctx.save()
+
+	# build up an appropriate font
+	ctx.select_font_face(face , cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+	ctx.set_font_size(font_size)
+	fascent, fdescent, fheight, fxadvance, fyadvance = ctx.font_extents()
+	x_off, y_off, tw, th = ctx.text_extents(string)[:4]
+	nx = -tw/2.0
+	ny = fheight/2
+
+	ctx.translate(pos[0], pos[1])
+	ctx.rotate(theta)
+	ctx.translate(nx, ny)
+	ctx.move_to(0,0)
+	ctx.show_text(string)
+	ctx.restore()
 
 
-def drawGaugeBackground(drawingContext, intMin, intMax, strDescriptioin):
-	"""
-	Draw the gauge backgrounds
-	"""
+if __name__ == '__main__':
 
-	startX = 250
-	startY = 270
-	radius = 200
-
-
-	lineWidth = 4
-	drawingContext.set_source_rgb(0, 0, 0)
-	drawingContext.set_line_width(lineWidth)
-
-	#------------------------------------------
-	#Draw the arc
-
-	angleOffsetDeg = 50
-
-	angleStartSinusDeg = angleOffsetDeg
-	angleStartArcDeg = 270 - angleOffsetDeg
-	angleStartSinusRad = ((math.pi*2)/360) * angleStartSinusDeg
-	angleStartArcRad = ((math.pi*2)/360) * angleStartArcDeg
-
-	angleEndSinusDeg = -angleOffsetDeg
-	angleEndArcDeg = 270 + angleStartSinusDeg
-	angleEndSinusRad = ((math.pi*2)/360) * angleEndSinusDeg
-	angleEndArcRad = ((math.pi*2)/360) * angleEndArcDeg
-
-
-	drawingContext.arc(startX, startY, radius, angleStartArcRad, angleEndArcRad)
-	drawingContext.stroke()
-
-	#------------------------------------------
-	#Draw the lines and labels of the line
-	count = 4
-
-	angleSumDeg = 2 * angleOffsetDeg
-	angleDelta = angleSumDeg / (count * 5)
-	speedDelta = intMax / count
-	angleDeg = angleStartSinusDeg
-	speedLabel = 0
-
-	for idx in range(21):
-		rad = ((math.pi*2)/360) * angleDeg
-		
-		if int(idx/5) == idx/5:
-			length = 20
-			lineWidth = 6
-			label = str(int(speedLabel))
-			speedLabel = speedLabel + speedDelta
-		else:
-			length = 10
-			lineWidth = 4
-			label = ""
-
-		drawLine(drawingContext, startX, startY, radius, rad, length, lineWidth, label)
-		angleDeg = angleDeg - angleDelta
-
-	#------------------------------------------
-	#Write the description
-	drawText(drawingContext, strDescriptioin, (startX, startY - ((radius/8)*5)), 0, "Georgia", 32)
-
-
-	#------------------------------------------
-	#Write the unit
-	drawText(drawingContext, "[Mbps]", (startX, startY - ((radius/8)*5) + 30), 0, "Georgia", 20)
-
-	#------------------------------------------
-	#Write to the svg
-	context.stroke()
-
-
-
-# creating a SVG surface
-# here geek is file name & 700, 700 is dimension
-with cairo.SVGSurface("upload.svg", 500, 300) as surface:
-
-	# creating a cairo context object
-	context = cairo.Context(surface)
-
-	# setting scale of the context
-	#context.scale(700, 700)
-
-	#on_draw(context)
-	drawGaugeBackground(context, 0, 30, "Upload")
-
-# creating a SVG surface
-# here geek is file name & 700, 700 is dimension
-with cairo.SVGSurface("download.svg", 500, 300) as surface:
-
-	# creating a cairo context object
-	context = cairo.Context(surface)
-
-	# setting scale of the context
-	#context.scale(700, 700)
-	
-	#on_draw(context)
-	drawGaugeBackground(context, 0, 300, "Download")
-
-# printing message when file is saved
-print("Files Saved")
+	#Start the main programm
+	main()
