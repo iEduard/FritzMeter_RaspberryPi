12562d2647f94327a4388d349302ae8823f18ff8
Eduard Schmidt <Eduard.Schmidt@posteo.net>
2022-01-18 19:10:38 +0100

    simplified code

--- Software/Background_Light.py
+++ Software/Background_Light.py
@@ -1,18 +1,11 @@
 from gpiozero import Button, LED
 
 
-def ledToggle(hwLed):
-    hwLed.toggle
-
-
-
 #Initialisierung von GPIO27 als Button (Eingang)
-
 button = Button(27)
 
 #Initialisierung von GPIO17 als LED (Ausgang)
-
 led = LED(17)
 
 #Wen der Button gedrückt wird
-button.when_pressed = ledToggle(led)
+button.when_pressed = led.toggle
