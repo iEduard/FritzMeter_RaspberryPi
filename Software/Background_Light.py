5cd9e44664e2795326a4aa091c2eaefe3fdbf8b9
Eduard Schmidt <Eduard.Schmidt@posteo.net>
2022-01-20 22:01:55 +0100

    Updated the Backlight script and tested with the Raspberry pi 4

--- Software/Background_Light.py
+++ Software/Background_Light.py
@@ -1,11 +1,21 @@
 from gpiozero import Button, LED
+from time import sleep
+from signal import pause
 
-while True:
-    #Init the button
-    button = Button(27)
 
-    #Init the led
-    led = LED(17)
+def toggleLed():
+    """
+    Toggle the LED and delay with 200ms.
+    This should prevent the led to be turned off from an bouncing button or switch
+    """
 
-    #link the button pressed event to the led toggle function
-    button.when_pressed = led.toggle
+    led.toggle()
+    sleep(0.2)
+
+
+button = Button(2)  #Init the button
+led = LED(18)       #Init the led
+
+button.when_pressed = toggleLed #Link the toggleLed funktion to button pressed
+
+pause() #Keep the script running
